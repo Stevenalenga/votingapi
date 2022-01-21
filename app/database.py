2f8a7671bca297f2d69e4b38d3cf95b51bf663f9
Stevenalenga <steven.alenga@gmail.com>
2022-01-21 12:56:14 +0300

    database

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ from sqlalchemy.sql.expression import false
 from sqlalchemy.ext.declarative import declarative_base
 from .config import settings
 
-SQLALCHEMY_DATA_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_username}"
+SQLALCHEMY_DATA_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATA_URL)
 
